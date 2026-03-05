e4c23dd25aea58be4c5202e268152bee74f00a95
Kunj patel <264775416+rakshacyberforce@users.noreply.github.com>
2026-03-05 18:16:37 +0530

    Update photo_osint.sh

--- photo_osint.sh
+++ photo_osint.sh
@@ -8,9 +8,9 @@ NC='\033[0m'
 clear
 
 echo -e "${CYAN}"
-echo "====================================="
-echo "        PHOTO OSINT TOOL v3"
-echo "====================================="
+echo "======================================="
+echo "          PHOTO OSINT TOOL"
+echo "======================================="
 echo -e "${NC}"
 
 read -p "Enter image path: " image
@@ -19,10 +19,11 @@ mkdir -p results
 
 echo -e "${GREEN}[+] Extracting Metadata${NC}"
 
-camera=$(exiftool -model "$image" | awk -F': ' '{print $2}')
+camera=$(exiftool -Model "$image" | awk -F': ' '{print $2}')
 date=$(exiftool -DateTimeOriginal "$image" | awk -F': ' '{print $2}')
-lat=$(exiftool -gpslatitude "$image" | awk -F': ' '{print $2}')
-lon=$(exiftool -gpslongitude "$image" | awk -F': ' '{print $2}')
+
+lat=$(exiftool -n -gpslatitude "$image" | awk '{print $4}')
+lon=$(exiftool -n -gpslongitude "$image" | awk '{print $4}')
 
 echo "Camera Model : $camera" > results/info.txt
 echo "Date Taken   : $date" >> results/info.txt
@@ -41,12 +42,6 @@ echo ""
 echo -e "${CYAN}[+] Running Face Detection${NC}"
 
 python3 face_detect.py "$image"
-
-echo ""
-echo -e "${CYAN}[+] Reverse Image Search${NC}"
-
-echo "Google: https://images.google.com"
-echo "TinEye: https://tineye.com"
 
 echo ""
 echo -e "${GREEN}OSINT Scan Completed${NC}"
