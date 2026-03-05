925c7ea76ffafc9f32606b72d0ea1e9d88725ab3
Kunj patel <264775416+rakshacyberforce@users.noreply.github.com>
2026-03-05 18:22:49 +0530

    Update face_detect.py

--- face_detect.py
+++ face_detect.py
@@ -12,9 +12,10 @@ exit()
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 face_cascade = cv2.CascadeClassifier(
-cv2.data.haarcascades + 'haarcascade_frontalface_default.xml'
+cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
 )
 
 faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
 print("Faces detected:", len(faces))
+
